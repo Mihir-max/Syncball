2d332367e73d7fbd92b479b8bc8d328fa5fb429d
Mihir-max <anandmihir@gmail.com>
2021-05-01 12:57:58 +0530

    edited the files

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
 var ball;
+var db,pos;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    db=firebase.database();
+    var ballpos=db.ref('ball/position');
+    ballpos.on("value",readpos)
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+
+    if (pos!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
+    
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    db.ref('ball/position').set({
+        'x':pos.x+x,
+        'y':pos.y+y,
+    })
+}
+function readpos(data){
+    pos=data.val();
+    ball.x=pos.x;
+    ball.y=pos.y;
 }
